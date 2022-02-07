a7a0fc642c5bf9d453a31c219fb811e61ce080bf
sbaum2s <80587859+sbaum2s@users.noreply.github.com>
2022-02-07 09:49:05 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form"
+import React, { useState } from 'react'
+import CardList from "./components/CardList"
 
 function App() {
+  const [cards, setCards] = useState([])
+
+  const addNewCard = (card) => {
+    // add profile to array
+    //setCards([...cards, card])
+
+    // reset array with each new search
+    setCards([card])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+        Search a GitHub User
+      </h1>
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
     </div>
   );
 }
